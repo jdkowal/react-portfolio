9c35284fc9b2b64554cc2bc4f014f6ac9ea70087
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-12-09 21:41:28 -0600

    got some things to work pdf still does not render

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import chopsticks from './static/chopsticksNchill.png'
+
 
 export default function Portfolio() {
   return (
-    <div>
-      <h2>Work</h2>
-      <div>
-        <a href="https://kbonaccorsi.github.io/group-project-1/">
-          <div>
-            <h3>Chopsticks and Chill</h3>
-            <span>javascript</span>
-          </div>
-        </a>
-      </div>
-    </div>
+    <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+              <Grid xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image={chopsticks}
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Chopsticks and Chill
+                    </Typography>
+                    <Typography>
+                      Javascript and APIs
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Link to="https://kbonaccorsi.github.io/group-project-1/" >
+                      <Button size="small">Deployed</Button>
+                    </Link>
+                    <Link to="https://github.com/kbonaccorsi/group-project-1" >
+                      <Button size="small">GITHUB</Button>
+                    </Link>
+                  </CardActions>
+                </Card>
+              </Grid>
+          </Grid>
+        </Container>
   );
 }
